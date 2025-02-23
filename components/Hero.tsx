b08c0144892d733c0dfca27100ad8b32de74e318
ochoabryan <bochoa314@gmail.com>
2025-02-22 22:01:29 -0500

    installed theme provider, background lights, spotlight into hero section.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,17 @@
 import { Spotlight } from "./ui/spotlight-new";
+import { BackgroundLines } from "./ui/background-lines";
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
-        <div>
-            <Spotlight />
-        </div>
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight />
+      </div>
+      <div>
+        <BackgroundLines className="h-full w-full dark:bg-black-100 flex items-center justify-center flex-col px-4" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
