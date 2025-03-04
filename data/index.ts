92c44e290af479355bd05b05287ddc861c0acdd4
ochoabryan <bochoa314@gmail.com>
2025-03-04 06:28:14 -0500

    Added boiler plate for recent projects.

--- data/index.ts
+++ data/index.ts
@@ -81,11 +81,11 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
+      title: "The Love Calculator",
+      des: "Discover your love compatibility with this fun and interactive tool! Using a unique algorithm, it calculates the connection between two names and reveals your romantic potential.",
+      img: "/calc.svg",
       iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      link: "https://github.com/BryanOchoa/loveCalculator",
     },
     {
       id: 2,
